e882c60b05e211a36a42ff56e71b14623c9efbec
dghero <devingreyhero@gmail.com>
2018-09-04 11:33:33 -0700

    Reformat Open Address Hash into own file

--- week13d1/hash-maps.js
+++ week13d1/hash-maps.js
@@ -1,114 +1,27 @@
-class HashMap {
-    constructor(initialCapacity = 8) {
-        this.length = 0;
-        this._slots = [];
-        this._capacity = initialCapacity;
-        this._deleted = 0;
-    }
-
-    display() {
-        return this._slots.filter(slot => {
-            return (slot !== undefined && slot.deleted === false);
-        });
-    }
-
-    get(key) {
-        const index = this._findSlot(key);
-        if (this._slots[index] === undefined) {
-            throw new Error('Key error');
-        }
-        return this._slots[index].value;
-    }
-
-    set(key, value) {
-        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-            this._resize(this._capacity * HashMap.SIZE_RATIO);
-        }
-
-        const index = this._findSlot(key);
-        this._slots[index] = {
-            key,
-            value,
-            deleted: false
-        };
-        this.length++;
-    }
-
-    remove(key) {
-        const index = this._findSlot(key);
-        const slot = this._slots[index];
-        if (slot === undefined) {
-            throw new Error('Key error');
-        }
-        slot.deleted = true;
-        this.length--;
-        this._deleted++;
-    }
-
-    _findSlot(key) {
-        const hash = HashMap._hashString(key);
-        const start = hash % this._capacity;
-
-        for (let i = start; i < start + this._capacity; i++) {
-            const index = i % this._capacity;
-            const slot = this._slots[index];
-            if (slot === undefined || (slot.key == key && !slot.deleted)) {
-                return index;
-            }
-        }
-    }
-
-    _resize(size) {
-        const oldSlots = this._slots;
-        this._capacity = size;
-        // Reset the length - it will get rebuilt as you add the items back
-        this.length = 0;
-        this._deleted = 0;
-        this._slots = [];
-
-        for (const slot of oldSlots) {
-            if (slot !== undefined && !slot.deleted) {
-                this.set(slot.key, slot.value);
-            }
-        }
-    }
-
-    static _hashString(string) {
-        let hash = 5381;
-        for (let i = 0; i < string.length; i++) {
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            hash = hash & hash;
-        }
-        return hash >>> 0;
-    }
-}
-
-HashMap.MAX_LOAD_RATIO = 0.7;
-HashMap.SIZE_RATIO = 3;
+const OAHashMap = require('./OAHash');
 
 const main = () => {
     let lotrData = [
-        { Hobbit: "Bilbo" },
-        { Hobbit: "Frodo" },
-        { Wizard: "Gandolf" },
-        { Human: "Aragon" },
-        { Elf: "Legolas" },
-        { Maiar: "The Necromancer" },
-        { Maiar: "Sauron" },
-        { RingBearer: "Gollum" },
-        { LadyOfLight: "Galadriel" },
-        { HalfElven: "Arwen" },
-        { Ent: "Treebeard" }
+        { Hobbit: 'Bilbo' },
+        { Hobbit: 'Frodo' },
+        { Wizard: 'Gandolf' },
+        { Human: 'Aragon' },
+        { Elf: 'Legolas' },
+        { Maiar: 'The Necromancer' },
+        { Maiar: 'Sauron' },
+        { RingBearer: 'Gollum' },
+        { LadyOfLight: 'Galadriel' },
+        { HalfElven: 'Arwen' },
+        { Ent: 'Treebeard' }
     ];
-    let lotr = new HashMap();
+    let lotr = new OAHashMap();
     lotrData.forEach(element => {
         let key = Object.keys(element);
         lotr.set(key[0], element[key[0]]);
     });
     // console.log(lotr.get("Wizard"));
     console.log(lotr);
-}
+};
 
 // main();
 
@@ -118,7 +31,7 @@ const main = () => {
 //output is boolean whether string iteration is palindrome
 
 const palindrome = string => {
-    let stringHashMap = new HashMap();
+    let stringHashMap = new OAHashMap();
     let odd = 0;
     for (let i = 0; i < string.length; i++) {
         try {
@@ -130,8 +43,8 @@ const palindrome = string => {
                 odd++;
             }
             stringHashMap.set(string[i], charCount);
-        } catch {
-            stringHashMap.set(string[i], 1)
+        } catch (e){
+            stringHashMap.set(string[i], 1);
             odd ++;
         }
     }
@@ -141,9 +54,9 @@ const palindrome = string => {
     } else {
         return false;
     }
-}
+};
 
-// console.log(palindrome('racecr'));
+console.log(palindrome('acecarr'));
 
 //--------------ANAGRAM-------------------------------
 
@@ -151,17 +64,17 @@ const palindrome = string => {
 //output is an array of arrays that consist of anagrams
 
 const anagram = (array) => {
-    let wordHashMap = new HashMap();
+    let wordHashMap = new OAHashMap();
     array.forEach(word => {
         let sortedKey = word.split('').sort().join('');
         try {
             let valuesArray = wordHashMap.get(sortedKey);
-            valuesArray.push(word)
-        } catch {
-            wordHashMap.set(sortedKey, [word])
+            valuesArray.push(word);
+        } catch(e){
+            wordHashMap.set(sortedKey, [word]);
         }
-    })
+    });
     return wordHashMap.display();
-}
+};
 
 console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
